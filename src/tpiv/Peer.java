0cb7c9cc651e23697ec840fd184cc041b30ec259
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-09 17:53:58 -0300

    classes para o Peer e para o server

---
+++ src/tpiv/Peer.java
@@ -0,0 +1,67 @@
+package tpiv;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.Socket;
+import javax.json.Json;
+import java.io.IOException;
+
+public class Peer {
+
+    public static void main(String[] args) throws IOException, Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter username and port for this peer"); // "Informe seu nome de usuario e a porta : "
+        String setupValues[] = bufferedReader.readLine().split(" ");
+        ServerThread serverThread = new ServerThread(setupValues[1]);
+        serverThread.start();
+        new Peer().updateListenToPeers(bufferedReader, setupValues[0], serverThread);
+    }
+
+    public void updateListenToPeers(BufferedReader bufferedReader, String username, ServerThread serverThread) throws Exception {
+        System.out.println("> enter (space separated) hostname:porta");
+        System.out.println("peers to receive messages from (s to skip)");// \nInforme o hostname:porta dos seus remetentes( Digite PULAR para ir direto para o chat
+        String input = bufferedReader.readLine();
+        String inputValues[] = input.split(" ");
+        if (!input.equals("s")) {
+            for (int i = 0; i < inputValues.length; i++) {
+                String address[] = inputValues[i].split(":");
+                Socket socket = null;
+                try {
+                    socket = new Socket(address[0], Integer.valueOf(address[1]));
+                    new PeerThread(socket).start();
+                } catch (Exception e) {
+                    if (socket != null) {
+                        socket.close();
+                    } else {
+                        System.out.println("Invalid input. Skipping to next step.");
+                    }
+                }
+            }
+        }
+        communicate(bufferedReader, username, serverThread);
+    }
+
+
+    public void communicate(BufferedReader bufferedReader, String username, ServerThread serverThread) throws Exception {
+        try {
+            System.out.println("You can now communicate. E to exit, c to change");
+            boolean flag = true;
+            while (flag) {
+                String message = bufferedReader.readLine();
+                if (message.equals("e")) {
+                    flag = false;
+                    break;
+                } else if (message.equals("c")) {
+                    updateListenToPeers(bufferedReader, username, serverThread);
+                } else {
+                    StringWriter stringW = new StringWriter();
+                    Json.createWriter(stringW).writeObject(Json.createObjectBuilder().add("username", username).add("message", message).build());
+                    serverThread.sendMessage(stringW.toString());
+                }
+            }
+            System.exit(0);
+        } catch (Exception e) {
+        }
+    }
+}
---
+++ src/tpiv/PeerThread.java
@@ -0,0 +1,33 @@
+package tpiv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class PeerThread extends Thread {
+    private BufferedReader bufferedReader;
+
+    public PeerThread(Socket socket) throws IOException {
+        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public void run() {
+        boolean flag = true;
+        while (flag) {
+            try {
+                JsonObject jsonO = Json.createReader(bufferedReader).readObject();
+                if (jsonO.containsKey("username")) {
+                    System.out.println("[" + jsonO.getString("username") + "]" + jsonO.getString("message"));
+                }
+            } catch (Exception e) {
+                flag = false;
+                interrupt();
+            }
+
+        }
+    }
+}
+
